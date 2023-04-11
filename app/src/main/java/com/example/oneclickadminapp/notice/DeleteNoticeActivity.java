1b297a2b1d7e0e0de051877f131f550170722bee
AbhimanyuDeswal <abhimanyudeswal07@gmail.com>
2023-04-11 15:59:48 +0530

    completed 17th, setup delete notice layout

---
+++ app/src/main/java/com/example/oneclickadminapp/notice/DeleteNoticeActivity.java
@@ -0,0 +1,16 @@
+package com.example.oneclickadminapp.notice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.oneclickadminapp.R;
+
+public class DeleteNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_notice);
+    }
+}
